512fdf2c18cc85943b233d1d04e1bbe999ee7d34
Jerome Troy <jrtroy@udel.edu>
2022-03-18 18:33:12 -0400

    Built setup file for library compilation

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setting up extension to be 
+# importable into python
+from distutils.core import setup, Extension
+
+module1 = Extension("spam",
+        sources = ["spam.c"])
+
+setup(name="Spam",
+        version="1.0",
+        description="This is a demo package",
+        ext_modules=[module1])
